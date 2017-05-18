aa2e565a80c0086b5da25d20404ba33e01a65c3e
carmemias <info@carmemias.com>
2017-05-18 15:38:06 +0100

    updated FAQs messages and bulk messages

--- src/cm-faq-cpt-definition.php
+++ src/cm-faq-cpt-definition.php
@@ -98,3 +98,75 @@ function get_all_post_type_features($post_type = 'post', $excluded_features = ar
 	return $included_features;
 }
 
+add_filter( 'post_updated_messages',  __NAMESPACE__ . '\cm_faq_updated_messages' );
+/**
+ * FAQs update messages.
+ *
+ * @param array $messages Existing post update messages.
+ *
+ * @return array Amended post update messages with new CPT update messages.
+ */
+function cm_faq_updated_messages( $messages ) {
+	$post             = get_post();
+	$post_type        = get_post_type( $post );
+	$post_type_object = get_post_type_object( $post_type );
+
+	$messages['cm_class'] = array(
+		0  => '', // Unused. Messages start at index 1.
+		1  => __( 'FAQs updated.', 'faqs-functionality' ),
+		2  => __( 'Custom field updated.', 'faqs-functionality' ),
+		3  => __( 'Custom field deleted.', 'faqs-functionality' ),
+		4  => __( 'FAQs updated.', 'faqs-functionality' ),
+		/* translators: %s: date and time of the revision */
+		5  => isset( $_GET['revision'] ) ? sprintf( __( 'FAQ restored to revision from %s', 'faqs-functionality' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6  => __( 'FAQ published.', 'faqs-functionality' ),
+		7  => __( 'FAQ saved.', 'faqs-functionality' ),
+		8  => __( 'FAQ submitted.', 'faqs-functionality' ),
+		9  => sprintf(
+			__( 'FAQ scheduled for: <strong>%1$s</strong>.', 'faqs-functionality' ),
+			// translators: Publish box date format, see http://php.net/date
+			date_i18n( __( 'M j, Y @ G:i', 'faqs-functionality' ), strtotime( $post->post_date ) )
+		),
+		10 => __( 'FAQ draft updated.', 'faqs-functionality' )
+	);
+
+	if ( $post_type_object->publicly_queryable && 'cm_faq' === $post_type ) {
+		$permalink = get_permalink( $post->ID );
+
+		$view_link = sprintf( ' <a href="%s">%s</a>', esc_url( $permalink ), __( 'View FAQ', 'faqs-functionality' ) );
+		$messages[ $post_type ][1] .= $view_link;
+		$messages[ $post_type ][6] .= $view_link;
+		$messages[ $post_type ][9] .= $view_link;
+
+		$preview_permalink = add_query_arg( 'preview', 'true', $permalink );
+		$preview_link = sprintf( ' <a target="_blank" href="%s">%s</a>', esc_url( $preview_permalink ), __( 'Preview FAQ', 'faqs-functionality' ) );
+		$messages[ $post_type ][8]  .= $preview_link;
+		$messages[ $post_type ][10] .= $preview_link;
+	}
+
+	return $messages;
+}
+
+
+add_filter( 'bulk_post_updated_messages', __NAMESPACE__ . '\cm_faq_bulk_updated_messages', 10, 2 );
+/**
+ * FAQs bulk update messages.
+ * See https://themeforest.net/forums/thread/custom-post-type-change-delete-message/114401
+ * @param array $messages Existing post bulk update messages.
+ *
+ * @return array Amended post update messages with new CPT update messages.
+ */
+function cm_faq_bulk_updated_messages( $bulk_messages, $bulk_counts ) {
+
+    $bulk_messages['cm_faq'] = array(
+        'updated'   => __( '%s FAQ updated.', '%s FAQs updated.', $bulk_counts['updated'] ),
+        'locked'    => __( '%s FAQ not updated, somebody is editing it.', '%s FAQs not updated, somebody is editing them.', $bulk_counts['locked'] ),
+        'deleted'   => __( '%s FAQ permanently deleted.', '%s FAQs permanently deleted.', $bulk_counts['deleted'] ),
+        'trashed'   => __( '%s FAQ moved to the Bin.', '%s FAQs moved to the Bin.', $bulk_counts['trashed'] ),
+        'untrashed' => __( '%s FAQ restored from the Bin.', '%s FAQs restored from the Bin.', $bulk_counts['untrashed'] ),
+    );
+
+    return $bulk_messages;
+
+}
+
